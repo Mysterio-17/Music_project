6077f3fe7a4e09065a2cff863b8ffd38adec004a
Mradul Tiwari <mradultiwari1708@gmail.com>
2025-07-03 14:33:09 +0530

    final_changes

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ roomSockets(io);
 
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
 }).then(() => {
   console.log('✅ MongoDB Connected');
 }).catch((err) => {
